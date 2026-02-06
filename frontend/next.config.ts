3c41ceea4e984bc84413ca2878d764680b5b72f8
delatorrecj <carlosjericodelatorre@gmail.com>
2026-02-06 20:24:35 +0800

    fix: reconfigured to remove webpack

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["react-pdf", "pdfjs-dist"],
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    return config;
-  },
+  // webpack: (config) => {
+  //   config.resolve.alias.canvas = false;
+  //   return config;
+  // },
 };
 
 export default nextConfig;
